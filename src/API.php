70f64595848cd8723b96182c7c91c0f9908dead9
kohabi <kohabi.mail@gmail.com>
2014-01-29 15:55:03 -0800

    added API

---
+++ src/API.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Kohabi\USPS;
+
+use Guzzle\Http\ClientInterface;
+
+class API
+{
+    private $client;
+    private $user;
+    private $testing;
+
+    public function __construct(ClientInterface $client, $user, $testing)
+    {
+        $this->client = $client;
+        $this->user = $user;
+        $this->testing = $testing;
+    }
+
+    private function getURL()
+    {
+        $dll = $this->testing ? 'ShippingAPITest.dll' : 'ShippingAPI.dll';
+        return 'https://secure.shippingapis.com/' . $dll;
+    }
+
+    private function request($api, $xml)
+    {
+        $request = $this->client->get($this->getURL());
+        $request->getQuery()->set('API', $api);
+        $request->getQuery()->set('XML', $xml);
+        $response = $request->send();
+        $responseParser = new ResponseParser();
+        if ($response->isSuccessful()) {
+            $body = $response->getBody(true);
+            return $responseParser->parse($body);
+        }
+        throw new APIException('Request failed');
+    }
+
+    public function standardizeAddress(Address $address)
+    {
+        $requestMapper = new Mapper\StandardizeAddressRequest();
+        $responseMapper = new Mapper\StandardizeAddressResponse();
+        $xml = $requestMapper->map($this->user, $address);
+        $body = $this->request('Verify', $xml);
+        return $responseMapper->map($body);
+    }
+}
---
+++ tests/APITest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Kohabi\USPS;
+
+class APITest extends \PHPUnit_Framework_TestCase
+{
+    private $api;
+
+    protected function setUp()
+    {
+        $user = getenv('USPS_USER');
+        $client = new \Guzzle\Http\Client();
+        $client->setSslVerification(true, true, 2);
+        $this->api = new API($client, $user, $testing = true);
+    }
+
+    public function testStandardizeAddress()
+    {
+        $address = new Address();
+        $address->addLine('6406 Ivy Lane');
+        $address->setCity('Greenbelt');
+        $address->setState('MD');
+        $res = $this->api->standardizeAddress($address);
+        $this->assertEquals('6406 IVY LN', $res->getLine(0));
+        $this->assertEquals('GREENBELT', $res->getCity());
+        $this->assertEquals('MD', $res->getState());
+        $this->assertEquals('20770-1441', $res->getPostalCode());
+    }
+}
