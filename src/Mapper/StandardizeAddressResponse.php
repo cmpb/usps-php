729b9f97bc40a026775d852f4132b181cbf33e30
kohabi <kohabi.mail@gmail.com>
2014-01-29 16:11:55 -0800

    phpcs fixes for PSR1,PSR2

--- src/Mapper/StandardizeAddressResponse.php
+++ src/Mapper/StandardizeAddressResponse.php
@@ -12,8 +12,9 @@ class StandardizeAddressResponse
         $address->addLine($response->Address->Address2);
         $address->setState($response->Address->State);
         $address->setCity($response->Address->City);
-        $address->setPostalCode($response->Address->Zip5 . '-' .
-                                $response->Address->Zip4);
+        $address->setPostalCode(
+            $response->Address->Zip5 . '-' . $response->Address->Zip4
+        );
         return $address;
     }
 }
