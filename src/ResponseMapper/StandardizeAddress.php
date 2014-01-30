6af44022f6f19a664b5fa248d5d023d8466be4f1
kohabi <kohabi.mail@gmail.com>
2014-01-30 14:56:37 -0800

    updated ResponseMapper/StandardizeAddress to use setZip5 adn setZip4

--- src/ResponseMapper/StandardizeAddress.php
+++ src/ResponseMapper/StandardizeAddress.php
@@ -12,9 +12,8 @@ class StandardizeAddress
         $address->setLine1($response->Address->Address2);
         $address->setState($response->Address->State);
         $address->setCity($response->Address->City);
-        $address->setPostalCode(
-            $response->Address->Zip5 . '-' . $response->Address->Zip4
-        );
+        $address->setZip5($response->Address->Zip5);
+        $address->setZip4($response->Address->Zip4);
         return $address;
     }
 }
