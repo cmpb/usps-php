f08a877165b415a9f52cc51d119ea486d7173580
kohabi <kohabi.mail@gmail.com>
2014-02-06 19:16:27 -0800

    added RequestInterface

---
+++ src/Request/RequestInterface.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Kohabi\USPS\Request;
+
+interface RequestInterface
+{
+    /**
+     * @return string
+     */
+    function getApiName();
+
+    /**
+     * @return string
+     */
+    function getRootElementName();
+
+    /**
+     * @return bool
+     */
+    function requiresUserid();
+
+    /**
+     * @return bool
+     */
+    function requiresPassword();
+
+    /**
+     * @return bool
+     */
+    function supportsSecure();
+
+    /**
+     * @return bool
+     */
+    function supportsProduction();
+}
