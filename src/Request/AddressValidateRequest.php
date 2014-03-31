58852fca0c1d150f6e5c9f06ecc16c32ce59c080
kohabi <kohabi.mail@gmail.com>
2014-03-31 15:53:39 -0700

    added new tests to AddressValidateRequest

--- src/Request/AddressValidateRequest.php
+++ src/Request/AddressValidateRequest.php
@@ -2,8 +2,15 @@
 
 namespace Kohabi\USPS\Request;
 
+use Kohabi\USPS\Model\Address;
+
 class AddressValidateRequest implements RequestInterface
 {
+    /**
+     * @var array
+     */
+    private $addresses = array();
+
     public function getApiName()
     {
         return 'Verify';
@@ -34,8 +41,32 @@ class AddressValidateRequest implements RequestInterface
         return true;
     }
 
+    /**
+     * @param \Kohabi\USPS\Model\Address $address
+     */
+    public function addAddress(Address $address)
+    {
+        $this->addresses[] = $address;
+    }
+
     public function getXmlBody()
     {
-        return '';
+        $body = '';
+        foreach ($this->addresses as $address) {
+            $body .= $this->map($address);
+        }
+        return $body;
+    }
+
+    private function map(Address $address)
+    {
+        return '<Address>' .
+                   '<Address1>' . $address->getLine2() . '</Address1>' .
+                   '<Address2>' . $address->getLine1() . '</Address2>' .
+                   '<City>' . $address->getCity() . '</City>' .
+                   '<State>' . $address->getState() . '</State>' .
+                   '<Zip5>' . $address->getZip5() . '</Zip5>' .
+                   '<Zip4>' . $address->getZip4() . '</Zip4>' .
+               '</Address>';
     }
 }
--- tests/Request/AddressValidateRequestTest.php
+++ tests/Request/AddressValidateRequestTest.php
@@ -1,9 +1,15 @@
 <?php
 
-namespace Kohabi\USPS\Request;
+namespace Kohabi\USPS\Tests\Request;
+
+use Kohabi\USPS\Request\AddressValidateRequest;
+use Kohabi\USPS\Model\Address;
 
 class AddressValidateRequestTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var \Kohabi\USPS\Request\AddressValidateRequest
+     */
     private $request;
 
     protected function setUp()
@@ -11,36 +17,45 @@ class AddressValidateRequestTest extends \PHPUnit_Framework_TestCase
         $this->request = new AddressValidateRequest();
     }
 
-    public function testRequiresUserid()
+    public function testInterface()
     {
-        $this->assertEquals(true, $this->request->requiresUserid());
-    }
+        $this->assertInstanceOf(
+            '\Kohabi\USPS\Request\RequestInterface',
+            $this->request
+        );
 
-    public function testRequiresPassword()
-    {
-        $this->assertEquals(false, $this->request->requiresPassword());
-    }
+        $this->assertTrue($this->request->requiresUserid());
+        $this->assertFalse($this->request->requiresPassword());
+        $this->assertFalse($this->request->supportsSecure());
+        $this->assertTrue($this->request->supportsProduction());
 
-    public function testSupportsSecure()
-    {
-        $this->assertEquals(false, $this->request->supportsSecure());
-    }
-
-    public function testSupportsProduction()
-    {
-        $this->assertEquals(true, $this->request->supportsProduction());
-    }
-
-    public function testGetRootElementName()
-    {
         $this->assertEquals(
             'AddressValidateRequest',
             $this->request->getRootElementName()
         );
+
+        $this->assertEquals('Verify', $this->request->getApiName());
     }
 
-    public function testGetApiName()
+    public function testMapping()
     {
-        $this->assertEquals('Verify', $this->request->getApiName());
+        $a = new Address();
+        $a->setLine1('6406 Ivy Lane');
+        $a->setLine2('Apt 3');
+        $a->setCity('Greenbelt');
+        $a->setState('MD');
+
+        $this->request->addAddress($a);
+
+        $expected = '<Address>' .
+                        '<Address1>Apt 3</Address1>' .
+                        '<Address2>6406 Ivy Lane</Address2>' .
+                        '<City>Greenbelt</City>' .
+                        '<State>MD</State>' .
+                        '<Zip5></Zip5>' .
+                        '<Zip4></Zip4>' .
+                    '</Address>';
+
+        $this->assertEquals($expected, $this->request->getXmlBody());
     }
 }
